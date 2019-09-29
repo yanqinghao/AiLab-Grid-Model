c4b1ce736bc81458c0b2a4bb6514a32f48bd997e
YanQinghao <woshiyanqinghao@gmail.com>
2019-09-29 14:12:29 +0800

    two function

--- components/stream/GridPreStream.py
+++ components/stream/GridPreStream.py
@@ -8,47 +8,55 @@ from suanpan.app import app
 from suanpan.app.arguments import Json
 from utils.utils import reCalcHoleGrid, preMain
 from suanpan.storage import storage
+from suanpan.log import logger
 
 
 @app.input(Json(key="inputData1"))
-# @app.input(Image(key="inputData1", required=True))
-# @app.input(Json(key="points"))
-# @app.param(Float(key="heightReal", default=1.0289))
-# @app.param(Float(key="widthReal", default=1.0294))
 @app.output(Json(key="outputData1"))
 def GridPreStream(context):
 
     args = context.args
     inputData = args.inputData1
+    func = inputData["func"]
     imageUrl = inputData["image"]
-    points = inputData["points"]
-    heightReal = inputData["heightReal"]
-    widthReal = inputData["widthReal"]
+    if "points" in inputData.keys():
+        points = inputData["points"]
+    if "heightReal" in inputData.keys():
+        heightReal = inputData["heightReal"]
+    if "widthReal" in inputData.keys():
+        widthReal = inputData["widthReal"]
+    if "rectangle" in inputData.keys():
+        rectangle = inputData["rectangle"]
+
     imagePath = os.path.join("/spnext", imageUrl)
 
     storage.download(imageUrl, imagePath)
 
     inputFolder = os.path.split(imagePath)[0]
     img = os.path.split(imagePath)[1]
-    preMain(
-        imgpath=imagePath,
-        saved_path=os.path.join(inputFolder, "result_" + img),
-        model_path=os.path.join(
-            os.path.dirname(os.path.abspath(sys.argv[0])), "utils", "model"
-        ),
-        height_real=heightReal,
-        width_real=widthReal,
-    )
-    reCalcHoleGrid(
-        png_path=imagePath,
-        json_path=os.path.join(inputFolder, "result_" + img, "response.json"),
-        rectangle=[],
-        points=points,
-    )
+    if func == "preMain":
+        preMain(
+            imgpath=imagePath,
+            saved_path=os.path.join(inputFolder, "result_" + img),
+            model_path=os.path.join(
+                os.path.dirname(os.path.abspath(sys.argv[0])), "utils", "model"
+            ),
+            height_real=heightReal,
+            width_real=widthReal,
+        )
+    elif func == "reCalcHoleGrid":
+        reCalcHoleGrid(
+            png_path=imagePath,
+            json_path=os.path.join(inputFolder, "result_" + img, "response.json"),
+            rectangle=rectangle,
+            points=points,
+        )
+    else:
+        logger.error("Undefined Function")
+        raise Exception("Undefined Function")
 
     storage.upload(
-        os.path.join(imageUrl, "result_" + img),
-        os.path.join(inputFolder, "result_" + img),
+        os.path.join(imageUrl, "result"), os.path.join(inputFolder, "result_" + img)
     )
 
     outputData = {"files": os.path.join(imageUrl, "result_" + img)}
